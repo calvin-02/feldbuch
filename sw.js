49dd2a69837fdb4afa71dcbdc6553ee7fc166348
calvin-02 <326265912+calvin-02@users.noreply.github.com>
2026-09-11 11:43:30 +0200

    Codelisten gegen die Projektmaske abgeglichen: Vitalitaet und Schaeden berichtigt (2.1.2)

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
    alles neu geladen und der alte Speicher gelöscht.
    --------------------------------------------------------------------------- */
 
-const FASSUNG = "feldbuch-2.1.1";
+const FASSUNG = "feldbuch-2.1.2";
 const DATEIEN = [
   "./",
   "./index.html",
