fe2f5ae48e7dd8b068afa521d2bee733001af0f4
calvin-02 <326265912+calvin-02@users.noreply.github.com>
2026-09-11 10:23:42 +0200

    Aufnahmeumfang ganzes Gatter statt 15-m-Kreis; Distanzgrenze aus gemessener Geometrie, nie aus Normmassen (2.1.1)

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
    alles neu geladen und der alte Speicher gelöscht.
    --------------------------------------------------------------------------- */
 
-const FASSUNG = "feldbuch-2.1.0";
+const FASSUNG = "feldbuch-2.1.1";
 const DATEIEN = [
   "./",
   "./index.html",
